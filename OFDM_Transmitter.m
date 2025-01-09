5b74223c40d776ac13b68e3e7181edd3f54da3ff
Mind18 <kacper.karting18@gmail.com>
2025-01-09 18:14:04 +0100

    Dodano część nadajnika związaną z IFFT

--- OFDM_Transmitter.m
+++ OFDM_Transmitter.m
@@ -1,16 +1,18 @@
-function ofdm_signal = OFDM_Transmitter(input_code, A, fc)
+function ofdm_signal = OFDM_Transmitter(input_code, n, A, fc)
 %OFDM_TRANSMITTER Function reprezents the OFDM modulation transmitter
 
-% ifft_output = 
+input_bins = qamCoder(input_code, n);
 
-ifft_re = randn(1, length(input_code)); % real(ifft_output);
-ifft_im = randn(1, length(input_code)); % imag(ifft_output);
+ifft_output = ifft(input_bins);
+
+ifft_re = real(ifft_output); % randn(1, length(input_code));
+ifft_im = imag(ifft_output); % randn(1, length(input_code));
 
 % re_carrier = A.*sin(2*pi*fc*(1:1000));
 % im_carrier = A.*cos(2*pi*fc*(1:1000));
 
-re_carrier = A.*sin(2*pi*(1/fc)*(1:length(ifft_re)));
-im_carrier = A.*cos(2*pi*(1/fc)*(1:length(ifft_im)));
+re_carrier = A.*sin(2*pi*(1/fc)*(1:size(ifft_re, 1)));
+im_carrier = A.*cos(2*pi*(1/fc)*(1:size(ifft_im, 1)));
 
 % Wykres do testów
 % figure(1);
@@ -19,24 +21,30 @@ im_carrier = A.*cos(2*pi*(1/fc)*(1:length(ifft_im)));
 % plot(1:length(im_carrier), im_carrier);
 % legend("Re carrier", "Im carrier");
 
-modulated_re = ifft_re.*re_carrier;
-modulated_im = ifft_im.*im_carrier;
+modulated_re = ifft_re.*re_carrier.';
+modulated_im = ifft_im.*im_carrier.';
 
-%Wykres do testów
-% figure(1);
-% plot(1:length(modulated_re), modulated_re);
-% hold on;
-% plot(1:length(ifft_re), ifft_re);
-% hold on;
-% plot(1:length(re_carrier), re_carrier);
-% legend("modulated re", "ifft re");
+% for i=1:size(ifft_re, 2)
+%     modulated_re = (ifft_re(:, i).').*re_carrier;
+%     modulated_im = (ifft_im(:, i).').*im_carrier;
 % 
-% figure(2);
-% plot(1:length(modulated_im), modulated_im);
-% hold on;
-% plot(1:length(ifft_im), ifft_im);
-% legend("modulated re", "ifft re");
+%     %Wykres do testów
+%     figure(2*i-1);
+%     plot(1:length(modulated_re), modulated_re);
+%     hold on;
+%     plot(1:length(ifft_re), ifft_re(:, i));
+%     hold on;
+%     plot(1:length(re_carrier), re_carrier);
+%     legend("modulated re", "ifft re");
+% 
+%     figure(2*i);
+%     plot(1:length(modulated_im), modulated_im);
+%     hold on;
+%     plot(1:length(ifft_im), ifft_im(:, i));
+%     legend("modulated im", "ifft im");
+% end
 
-ofdm_signal = modulated_re + modulated_im;
+modulated_combined = modulated_re + modulated_im;
+ofdm_signal = reshape(modulated_combined, [numel(modulated_combined) 1]).';
 end
 
--- test.m
+++ test.m
@@ -7,17 +7,36 @@ text = 'Aasdsa'
 % kod Morse'a to 1 dla kropki, 111 dla kreski, 0 pomiędzy nimi, 000
 % pomiędzy literami i 00000000 pomiędzy słowami (spacja)
 
-
 y_binary_Morse = textToBinaryMorse(text);
 
-qamOutput = qamCoder(y_binary_Morse, 2);
+qamOutput = qamCoder(y_binary_Morse, 8);
 
-qamSingal = ifft(qamOutput, width(qamOutput), 1);
+qamSignal = ifft(qamOutput);
 
-plot(qamSingal)
+re_qamSignal = real(qamSignal);
+im_qamSignal = imag(qamSignal);
+
+% plot(qamSignal);
+% for i=1:width(re_qamSignal)
+%     figure;
+%     signal_to_plot = re_qamSignal(:, i);
+%     plot(1:length(signal_to_plot), signal_to_plot);
+% end
+% 
+% for i=1:width(im_qamSignal)
+%     figure;
+%     signal_to_plot = im_qamSignal(:, i);
+%     plot(1:length(signal_to_plot), signal_to_plot);
+% end
+
 % stairs(y_binary_Morse)
 % ylim([-0.5, 1.5])
 
-%ofdm_signal = OFDM_Transmitter(y_binary_Morse, 2, 10);
+ofdm_signal = OFDM_Transmitter(y_binary_Morse, 8, 2, 10);
+% Dodanie zakłócenia
+
+% wrzucenie do Receivera
+
+% dekodowanie an tekst
 
 % text_rev = binaryMorseToText(y_binary_Morse)
