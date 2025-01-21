76f39acff65616712d2ca8b9beda495643d05702
Mind18 <kacper.karting18@gmail.com>
2025-01-21 16:24:03 +0100

    Added clear comments

--- OFDM_Receiver.m
+++ OFDM_Receiver.m
@@ -1,11 +1,17 @@
-function received_binary = OFDM_Receiver(received_signal_re, received_signal_im, n, A, fc)
+function received_binary = OFDM_Receiver(received_signal_re, received_signal_im, n)
 %OFDM_RECEIVER Function reprezents the OFDM modulation receiver
 
+% Złożenie sygnału z toru rzeczywistego i urojonego
 complex_signal = complex(received_signal_re, received_signal_im);
+
+% Podział sygnału na koszyki na potrzeby zrównoleglenia
 signal_sizes = [n, round(length(complex_signal) / n)];
 signal_framed = reshape(complex_signal, signal_sizes);
 
+% FFT koszyków
 fft_bins = fft(signal_framed, n, 1);
+
+% Dekodowanie znaków QAM na kod binarny
 received_binary = qamDecoder(fft_bins);
 end
 
--- OFDM_Transmitter.m
+++ OFDM_Transmitter.m
@@ -1,15 +1,18 @@
-function [ofdm_signal_re, ofdm_signal_im] = OFDM_Transmitter(input_code, n, A, fc)
+function [ofdm_signal_re, ofdm_signal_im] = OFDM_Transmitter(input_code, n)
 %OFDM_TRANSMITTER Function reprezents the OFDM modulation transmitter
 
+% Kodowanie sygnału w modulacji 4QAM
 input_bins = qamCoder(input_code, n);
 
+% IFFT zakodowanego sygnału
 ifft_output = ifft(input_bins);
 
+% Rozbicie na tor liczb rzeczywistych i urojonych
 ifft_re = real(ifft_output);
 ifft_im = imag(ifft_output);
 
+% Ułożenie w pojedynczy wektor do wysłania
 ofdm_signal_re = reshape(ifft_re, [numel(ifft_re) 1]).';
 ofdm_signal_im = reshape(ifft_im, [numel(ifft_im) 1]).';
-
 end
 
